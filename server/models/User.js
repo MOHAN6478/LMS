27beed3fc6a37980f86f1d8992dd32f44b2c7483
MOHAN6478 <mohan.m6478@gmailcom>
2025-05-20 20:40:17 +0530

    stripe added

--- server/models/User.js
+++ server/models/User.js
@@ -15,6 +15,6 @@ const userSchema = new mongoose.Schema(
     },{ timestamps : true}
 )
 
-const User = mongoose.models.user || mongoose.model('user',userSchema)
+const User = mongoose.models.user || mongoose.model('User',userSchema)
 
 export default User;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/db.js'
-import { clerkWebhooks } from './controllers/webhooks.js'
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js'
+import educatorRouter from './routes/educatorRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
+import { connectCloudinary } from './configs/cloudinary.js'
+import courseRouter from './routes/courseRoute.js'
+import userRouter from './routes/userRoute.js'
 
 // Initialize Express
 const app = express()
@@ -10,14 +15,20 @@ const port = process.env.PORT || 5000
 
 // Connect to database
 await connectDB()
+await connectCloudinary()
 
 // Middleware
 app.use(cors())
 app.use(express.json())
+app.use(clerkMiddleware())
 
 // Routes
 app.get('/', (req,res) => res.send('API working'))
 app.post('/clerk', clerkWebhooks)
+app.use('/api/educator',educatorRouter)
+app.use('/api/course',courseRouter)
+app.use('/api/user',userRouter)
+app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
 
 
 app.listen(port, () => console.log(`Sever is running http://localhost:${port}`))
