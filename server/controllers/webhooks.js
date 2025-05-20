d0a794807022e1718bbf5d58dca144e83dfed55d
MOHAN6478 <mohan.m6478@gmailcom>
2025-05-20 21:32:37 +0530

    stripeInstance changed

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -55,7 +55,7 @@ export const clerkWebhooks = async (req,res) => {
     }
 }
 
-const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
+const stripeInstance = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 export const stripeWebhooks = async (req,res) => {
     const sig = req.headers['stripe-signature'];
